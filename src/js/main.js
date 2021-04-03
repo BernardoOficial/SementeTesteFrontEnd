9d7c32c5bc74fb4908dbc24eaf201815d882c4d7
Bernardo Pereira Oliveira <bernardo.pereira@foster.com.br>
2021-04-03 16:03:26 -0300

    Finalizando o header e o footer do site

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 // Javascript aqui
+const iconMenu = document.querySelector("[data-icon]");
+const overlay = document.querySelector("[data-overlay]");
+const menuMobile = document.querySelector("[data-menu-mobile]");
+
+const toggleIcon = () => {
+    iconMenu.classList.add("header__icon-toggle--ativo");
+    overlay.classList.add('header__overlay--ativo');
+    menuMobile.classList.add('header__menu-mobile--ativo');
+}
+
+const hideOverlay = evento => {
+    evento.stopPropagation();
+    iconMenu.classList.remove("header__icon-toggle--ativo");
+    overlay.classList.remove('header__overlay--ativo');
+    menuMobile.classList.remove("header__menu-mobile--ativo");
+}
+
+menuMobile.addEventListener('click', evento => evento.stopPropagation());
+
+iconMenu.addEventListener('click', toggleIcon);
+overlay.addEventListener('click', hideOverlay);
